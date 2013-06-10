56b8917e25667b4e00def37f0809d4e4a8c4b240
Arnold Noronha <arnstein87@gmail.com>
2013-06-10 18:04:11 -0400

    basic version

---
+++ namespace.el
@@ -0,0 +1,39 @@
+
+
+(defun namespace-starts-with (s arg)
+      "returns non-nil if string S starts with ARG.  Else nil."
+      (cond ((>= (length s) (length arg))
+             (string-equal (substring s 0 (length arg)) arg))
+            (t nil)))
+
+(defun namespace-rename (namespace x)
+  (cond ((listp x) (mapcar (lambda (y) (namespace-rename namespace y)) x))
+        ((symbolp x) 
+         (if (namespace-starts-with (symbol-name x) "::")
+             (intern (concat namespace (symbol-name x)))
+           x))
+         (t x)))
+
+(defmacro in-namespace (namespace &rest body)
+  (let* ((form2 (cons 'progn body)))
+    (mapcar (lambda (y) (namespace-rename namespace y)) form2)))
+
+
+(macroexpand-1 (defun ff (x) (+ 1 x)))
+
+(macroexpand (in-namespace
+ "foo"
+ (defun ::xyz (x)
+   (+ 1 x))
+ (mapcar '::xyz '(1 2 3))))
+
+(macroexpand (in-namespace "foo" ::xyz))
+
+
+
+
+
+
+
+
+

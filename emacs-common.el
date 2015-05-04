678b9d6d65abb8f8191f2ee8c40db5624eab1f7c
Arnold Noronha <arnold@fb.com>
2015-05-04 11:33:12 -0400

    revert-all-buffer

--- emacs-common.el
+++ emacs-common.el
@@ -248,3 +248,12 @@ mentioned in an erc channel" t)
                                   java-mode-syntax-table)))
 
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+  (defun revert-all-buffers ()
+    "Refreshes all open buffers from their respective files."
+    (interactive)
+    (dolist (buf (buffer-list))
+      (with-current-buffer buf
+        (when (and (buffer-file-name) (file-exists-p (buffer-file-name)) (not (buffer-modified-p)))
+          (revert-buffer t t t) )))
+    (message "Refreshed open files.") )

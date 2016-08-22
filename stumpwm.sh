c390144cdbb918e4979fb6861e54672a6a9b041e
Arnold Noronha <arnold@fb.com>
2016-08-22 18:14:55 -0400

    ...

---
+++ stumpwm.sh
@@ -0,0 +1,12 @@
+gnome-settings-daemon &
+nm-applet --sm-disable &
+
+eval $(/usr/bin/gnome-keyring-daemon --daemonize --login --start --components=gpg,pkcs11,secrets,ssh)
+export SSH_AUTH_SOCK
+export GPG_AGENT_INFO
+export GNOME_KEYRING_CONTROL
+export GNOME_KEYRING_PID
+
+
+~arnold/builds/stumpwm/stumpwm
+
